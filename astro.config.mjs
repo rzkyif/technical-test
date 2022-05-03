0d125410b33dc7ef137133fadc61acdf1fe11a37
Rizky I. F <rykzius@gmail.com>
2022-05-03 19:32:28 +0700

    Add deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rzkyif.github.io",
+  base: "/technical-test",
   integrations: [solid(), tailwind()]
 });
