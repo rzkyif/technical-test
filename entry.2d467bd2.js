1fb3b03947469206c9dc18cccb79fce97eabfa49
Rizky I. F <rykzius@gmail.com>
2022-05-03 20:17:26 +0700

    deploy

---
+++ entry.2d467bd2.js
@@ -0,0 +1,1 @@
+import{k as o,h as i,s as l}from"./chunks/chunk.329a80b1.js";var s=t=>(n,a,r)=>{window._$HY||(window._$HY={events:[],completed:new WeakSet,r:{}});let e;o(()=>i(n,{...a,get children(){return r!=null&&(l.context&&(e=t.querySelector("astro-fragment")),e==null&&(e=document.createElement("astro-fragment"),e.innerHTML=r)),e}}),t)};export{s as default};
